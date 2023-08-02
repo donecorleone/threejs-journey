3f2c29cd72c4cca34bf6dc7c51100c1e1d087ec3
donecorleone <88971777+donecorleone@users.noreply.github.com>
2023-08-02 11:57:52 +0200

    x
    x

--- 20-scroll-models/src/script.js
+++ 20-scroll-models/src/script.js
@@ -1,51 +1,85 @@
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
+
+const gltfLoader = new GLTFLoader()
 
 THREE.ColorManagement.enabled = false
 
-/**
- * Debug
- */
-const gui = new dat.GUI()
-
-const parameters = {
-    materialColor: '#ffeded'
+const cursor = {
+    x: 0,
+    y: 0
 }
-
-gui
-    .addColor(parameters, 'materialColor')
+window.addEventListener('mousemove', (event) => 
+{
+    cursor.x = event.clientX / sizes.width -0.5
+    cursor.y = -(event.clientY / sizes.width -0.5)
+    
+})
 
 /**
  * Base
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+const wrapper = document.querySelector('.wrapper')
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// Model
+
+let model;
+
+gltfLoader.load(
+    '/models/Balloon/scene.gltf',
+    (gltf) =>
+    {
+        model = gltf.scene
+        scene.add(model)
+        model.scale.set(0.1, 0.1, 0.1)
+        model.position.set(0, 1.5, 0)
+        model.rotation.set(0, 1, 0)
+       
+
+    }
 )
-scene.add(cube)
+
+// Lights
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: wrapper.offsetWidth,
+    height: wrapper.offsetHeight
 }
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = wrapper.offsetWidth
+    sizes.height = wrapper.offsetHeight
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -60,9 +94,17 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
 camera.position.z = 6
+
 scene.add(camera)
+
+// Controls
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -83,6 +125,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+      // Update objects
+    
+
+    // Update Camera
+   
+
+    // model.rotation.x = .5 * elapsedTime
+    // model.rotation.y = .2 * elapsedTime
 
     // Render
     renderer.render(scene, camera)
