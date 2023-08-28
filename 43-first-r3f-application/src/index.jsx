378cbdfa357ae8e0042dc518d129c0e2b4b6fa8b
donecorleone <88971777+donecorleone@users.noreply.github.com>
2023-08-28 17:32:56 +0200

    x
    x

--- 43-first-r3f-application/src/index.jsx
+++ 43-first-r3f-application/src/index.jsx
@@ -1,10 +1,17 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { useGLTF, Float, Lightformer, Text, Html, ContactShadows, Environment, MeshTransmissionMaterial } from "@react-three/drei"
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
-        Soon to be a badass R3F application
+      <Canvas>
+        <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="red" />
+        </mesh>
+      </Canvas>
     </>
 )
